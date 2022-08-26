99f5da83e36d0868002bb413be0f6563e1c78d98
Seungbaek Kim <kimv100@gmail.com>
2022-08-26 11:38:32 +0900

    initial

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+    language: "ko-KR",
+    // language: "en-US",
+  },
+});
+
+export default instance;
---
+++ src/components/Nav.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Nav.css";
+
+export default function Nav() {
+  const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log("window.scrollY", window.scrollY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
+  return (
+    <nav className={`nav ${show && "nav__black"} `}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => (window.location.href = "/")}
+      />
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
+      />
+      <img 
+        alt="User logged"
+        src="https://lh3.googleusercontent.com/ogw/AOh-ky1qnVdA0XGuU7hHjUWngqxT7Ryeyec8A-N1azD-N9s=s32-c-mo"
+        className='nav__avatar'
+      />
+    </nav>
+  );
+}
